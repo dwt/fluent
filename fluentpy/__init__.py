cfbbc26b61114454973a5cebc4ca70e9eb93a734
Martin Häcker <mhaecker@mac.com>
2019-01-01 10:06:32 +0100

    Rework the executable wrapper to import less symbols directly.
    This was an experiment to make the module reloadable, which failed for now as functools.wraps anyway caches all the symbols on the executable wrapper. Still better code me thinks though.

--- fluentpy/__init__.py
+++ fluentpy/__init__.py
@@ -31,27 +31,24 @@ which order more clearly.
 For further documentation and development see this documentation or the source at https://github.com/dwt/fluent
 """
 
-from fluentpy.wrapper import __all__, __api__, _wrap_alternatives
-
-import fluentpy.wrapper as wrapper
-for name in __all__ + __api__:
-    locals()[name] = getattr(wrapper, name)
-del name
-# del wrapper
+import fluentpy.wrapper
 
 # Make the module executable via `python -m fluentpy "some fluent using python code"`
 
 import functools, sys
-@functools.wraps(wrap)
-def executable_module(*args, **kwargs): return wrap(*args, **kwargs)
-executable_module.__name__ = __name__ + '.' + wrap.__name__
+# REFACT What would be the best way not to cache all the attributes of fluentpy.wrapperr with functools.wrraps
+@functools.wraps(fluentpy.wrapper.wrap)
+def executable_module(*args, **kwargs): return fluentpy.wrapper.wrap(*args, **kwargs)
+executable_module.__name__ = __name__ + '.' + fluentpy.wrapper.wrap.__name__
 executable_module.module = sys.modules[__name__]
 executable_module.__package__ = __package__
-executable_module.__all__ = __all__
-executable_module.__api__ = __api__
+executable_module.__all__ = fluentpy.wrapper.__all__
+executable_module.__api__ = fluentpy.wrapper.__api__
 executable_module.__file__ = __file__
 executable_module.__doc__ = __doc__
 executable_module.__path__ = __path__
-_wrap_alternatives.append(executable_module)
+# REFACT is there a way to formulate this so that reloading the module doesn't break it?
+# That would probably mean recognizing all module wrappers by some of their attributes instead of by identity
+fluentpy.wrapper._wrap_alternatives.append(executable_module)
 sys.modules[__name__] = executable_module
 # _wrapper_is_sealed = True
